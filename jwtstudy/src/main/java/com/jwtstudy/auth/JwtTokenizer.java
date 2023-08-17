2a435e5b3922e62b946f23c3946a12053d4c2e4b
yawn <yawning055@gmail.com>
2023-08-18 00:32:50 +0900

    토큰 검증기능 및 테스트 작성완료

--- jwtstudy/src/main/java/com/jwtstudy/auth/JwtTokenizer.java
+++ jwtstudy/src/main/java/com/jwtstudy/auth/JwtTokenizer.java
@@ -13,6 +13,17 @@ import java.util.Map;
 
 public class JwtTokenizer { // jwt 생성하는 클래스
 
+    // JWT의 서명에 사용할 Secret Key를 생성
+    private Key getKeyFromBase64EncodedKey(String base64EncodedSecretKey) {
+        // Base64형식으로 인코딩 된 SecretKey를 디코딩 한 후, byte array반환.
+        byte[] keyBytes = Decoders.BASE64.decode(base64EncodedSecretKey);
+        // key byte array를 기반으로 적절한 HMAC 알고리즘을 사용한
+        // Key(java.Security.Key)객체 생성
+        Key key = Keys.hmacShaKeyFor(keyBytes);
+
+        return key;
+    }
+
     // Plain Text의 secretKey를 Base64로 암호화
     public String encodedBase64SecretKey(String secretKey) {
         return Encoders.BASE64.encode(secretKey.getBytes(StandardCharsets.UTF_8));
@@ -42,6 +53,7 @@ public class JwtTokenizer { // jwt 생성하는 클래스
                 .compact();
     }
 
+    // refresh 토큰을 생성해주는 메서드
     public String generateRefreshToken(String subject, Date expiration, String base64EncodedSecretKey) {
         Key key = getKeyFromBase64EncodedKey(base64EncodedSecretKey);
 
@@ -55,23 +67,26 @@ public class JwtTokenizer { // jwt 생성하는 클래스
                 .compact();
     }
 
-    // JWT의 서명에 사용할 Secret Key를 생성
-    private Key getKeyFromBase64EncodedKey(String base64EncodedSecretKey) {
-        // Base64형식으로 인코딩 된 SecretKey를 디코딩 한 후, byte array반환.
-        byte[] keyBytes = Decoders.BASE64.decode(base64EncodedSecretKey);
-        // key byte array를 기반으로 적절한 HMAC 알고리즘을 사용한
-        // Key(java.Security.Key)객체 생성
-        Key key = Keys.hmacShaKeyFor(keyBytes);
-
-        return key;
-    }
-
+    // jwt 검증관련 메서드
+    // jjwt(java Jwt)에서는 jwt를 생성할 때 서명에 사용된 SecretKey를 이용해 내부적으로
+    // Signature를 검증한 후, 검증에 성공하면 JWT를 파싱 해서 Claims(인증된 사용자 정보)를 얻을 수 있다.
+    // 인자로는 jws = 완전한 jwt 토큰, 해당 jwt토큰을 생성할때 사용한 비밀키 = base64...key
     public void verifySignature(String jws, String base64EncodedSecretkey) {
+        // Base64로 인코딩된 비밀키를 실제 키 객체로 반환한다.
         Key key = getKeyFromBase64EncodedKey(base64EncodedSecretkey);
 
         Jwts.parserBuilder()
+                // 서명에 사용된 Secret Key 설정
+                // key는 jwt가 생성될 때 사용된 것과 동일한 키여야함
+                // 다른 키면 검증 실패
                 .setSigningKey(key)
+                // 설정된 정보를 바탕으로 실제 jwt를 파싱하는 객체 생성
                 .build()
+                // jwt를 파싱해서 claims를 얻는다(jwt의 페이로드 부분)
+                // parseClaimsJws(jws) 호출 과정에서 내부적으로 JWT의 시그니처를 검증하게 됩니다.
+                // 이때, 앞서 설정된 비밀키 .setSigningKey(key)가 사용됩니다.
+                // 시그니처 잘못됨 or jwt변조면 예외 발생
+                // 여기서 받는 jws는 완전한 jwt 토큰이여야함.
                 .parseClaimsJws(jws);
     }
 }
--- jwtstudy/src/test/java/com/jwtstudy/JwtTokenizerTest.java
+++ jwtstudy/src/test/java/com/jwtstudy/JwtTokenizerTest.java
@@ -1,16 +1,21 @@
 package com.jwtstudy;
 
 import com.jwtstudy.auth.JwtTokenizer;
+import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.io.Decoders;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 
 import java.util.*;
+import java.util.concurrent.TimeUnit;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.notNullValue;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class JwtTokenizerTest {
@@ -42,7 +47,9 @@ public class JwtTokenizerTest {
         claims.put("roles", List.of("USER"));
 
         String subject = "test access token";
+        // 현재 날짜와 시간으로 초기화 된 calendar 객체를 가져옴
         Calendar calendar = Calendar.getInstance();
+        // 현재 시간에 10분을 더함
         calendar.add(Calendar.MINUTE, 10);
         Date expiration = calendar.getTime();
         // 위의 정보를 바탕으로 jwt토큰 생성
@@ -56,7 +63,9 @@ public class JwtTokenizerTest {
     @Test
     public void generateRefreshTokenTest() {
         String subject = "test refresh token";
+        // 현재 날짜와 시간으로 초기화 된 calendar 객체를 가져옴
         Calendar calendar = Calendar.getInstance();
+        // 현재 시간에 24시간을 더함
         calendar.add(Calendar.HOUR, 24);
         Date expiration = calendar.getTime();
 
@@ -66,5 +75,45 @@ public class JwtTokenizerTest {
         System.out.println(refreshToken);
 
         assertThat(refreshToken, notNullValue());
+    }
+
+    // jwt에서 쓰는 Key 객체를 얻는 과정
+    private String getAccessToken(int timeUnit, int timeAmount) {
+        Map<String, Object> claims = new HashMap<>();
+        claims.put("memberId", 1);
+        claims.put("roles", List.of("USER"));
+
+        String subject = "test access token";
+        Calendar calendar = Calendar.getInstance();
+        calendar.add(timeUnit, timeAmount);
+        Date expiration = calendar.getTime();
+
+        // 위의 설정들은 전부다 jwt를 얻기위해 들어갈 인자들을 설정해준 것 뿐이다.
+        // 여기서 위의 정보를 기반으로 한 jwt생성
+        String accessToken = jwtTokenizer.generateAccessToken(claims, subject, expiration, base64EncodedSecretKey);
+
+        return accessToken;
+    }
+
+    // JwtTokenizer 의 verifySignature() 메서드가 Signature을 잘 검증하는지 테스트
+    // 예외 안 나면 잘 검증중인 것
+    @DisplayName("does not throw any Exception when jws verify")
+    @Test
+    public void verifySignatureTest() {
+        String accessToken = getAccessToken(Calendar.MINUTE, 10);
+        assertDoesNotThrow(() -> jwtTokenizer.verifySignature(accessToken, base64EncodedSecretKey));
+    }
+
+    // Jwt 생성 시 지정한 만료일시가 지나면 Jwt가 진짜 만료되는지 검증
+    // 예외 발생해야 잘 작동되고 있는 거임
+    @DisplayName("throw ExpiredJwtException when jws verify")
+    @Test
+    public void verifyExpirationTest() throws InterruptedException {
+        String accessToken = getAccessToken(Calendar.SECOND, 1);
+        assertDoesNotThrow(() -> jwtTokenizer.verifySignature(accessToken, base64EncodedSecretKey));
+
+        TimeUnit.MICROSECONDS.sleep(1500);
+
+        assertThrows(ExpiredJwtException.class, () -> jwtTokenizer.verifySignature(accessToken, base64EncodedSecretKey));
     }
 }
