a3328641c67d15bb0864a885ac7c05e9eef5f263
yawn <yawning055@gmail.com>
2023-08-17 17:04:16 +0900

    jwt 공부 시작

---
+++ jwtstudy/src/test/java/com/jwtstudy/JwtstudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jwtstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtstudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
