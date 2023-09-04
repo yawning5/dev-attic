6e49787764ef2089ec00982f2c2a19e21dc0dd72
yawn <yawning055@gmail.com>
2023-09-04 20:58:22 +0900

    Initial commit

---
+++ src/main/java/com/server/search/controller/dto/SearchKeyword.java
@@ -0,0 +1,12 @@
+package com.server.search.controller.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class SearchKeyword {
+	private String keyword;
+}
