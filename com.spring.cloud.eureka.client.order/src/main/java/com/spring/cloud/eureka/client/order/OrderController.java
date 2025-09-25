90bc60d6adfbcf52ef0d8700c96a623e44405451
yawning5 <yawning055@gmail.com>
2025-09-25 23:07:55 +0900

    FeignClient 적용

---
+++ com.spring.cloud.eureka.client.order/src/main/java/com/spring/cloud/eureka/client/order/OrderController.java
@@ -0,0 +1,18 @@
+package com.spring.cloud.eureka.client.order;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class OrderController {
+
+    private final OrderService orderService;
+
+    @GetMapping("/order/{orderId}")
+    public String getOrder(@PathVariable("orderId") String orderId) {
+        return orderService.getOrder(orderId);
+    }
+}
