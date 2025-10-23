890d11db696d352dda06ca5524db68532430f734
yawning5 <yawning055@gmail.com>
2025-10-23 15:29:22 +0900

    test

--- src/test/java/com/spring/sample/SampleApplicationTests.java
+++ src/test/java/com/spring/sample/SampleApplicationTests.java
@@ -2,7 +2,7 @@ package com.spring.sample;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
+//test
 @SpringBootTest
 class SampleApplicationTests {
 
