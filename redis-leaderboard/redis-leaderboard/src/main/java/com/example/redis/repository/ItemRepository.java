da4e2da1e38bd276bd8490e661686a980565358d
yawning5 <yawning055@gmail.com>
2026-02-07 13:19:41 +0900

    위치변경

--- redis-leaderboard/redis-leaderboard/src/main/java/com/example/redis/repository/ItemRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.redis.repository;
-
-import com.example.redis.domain.Item;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ItemRepository extends JpaRepository<Item, Long> { }
--- redis-leaderboard/redis-leaderboard/src/main/java/com/example/redis/repository/OrderRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.redis.repository;
-
-import com.example.redis.domain.ItemOrder;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderRepository extends JpaRepository<ItemOrder, Long> { }
